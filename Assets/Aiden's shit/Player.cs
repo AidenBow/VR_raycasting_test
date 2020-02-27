e63edab25a582edba8072e376734d86bde7825d2
AidenBow <AidenBow@users.noreply.github.com>
2020-02-26 22:25:49 -0800

    added evilman prefab and steamvr grip detection

--- Assets/Aiden's shit/Player.cs
+++ Assets/Aiden's shit/Player.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class Player : MonoBehaviour
 {
     public float playerSpeed = .25f;
     public Vector3 housePosition;
     public Vector3 targetPosition;
+    public SteamVR_Input_Sources handType;
     void Start()
     {
         targetPosition = gameObject.transform.parent.position;
@@ -27,6 +29,11 @@ public class Player : MonoBehaviour
 
         gameObject.transform.parent.position = Vector3.Lerp(gameObject.transform.parent.position, targetPosition, Time.deltaTime * playerSpeed);
 
+        bool gripActive = SteamVR_Actions.default_GrabGrip.GetState(handType);
+        if (gripActive)
+        {
+            Debug.Log("grabgrip");
+        }
     }
 
     private void MoveToHouse ()
