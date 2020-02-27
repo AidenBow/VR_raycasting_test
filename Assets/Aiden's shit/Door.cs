1eb60562df8657e6f3620c1f382010d29ac5b297
AidenBow <AidenBow@users.noreply.github.com>
2020-02-26 16:43:30 -0800

    door opens when looking at button

---
+++ Assets/Aiden's shit/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Vector3 loweredPosition;
+    public Vector3 targetPosition;
+    void Start()
+    {
+        targetPosition = gameObject.transform.localPosition; // keeps door in starting position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.localPosition = Vector3.Lerp(gameObject.transform.localPosition, targetPosition, Time.deltaTime); //moves door to target position
+    }
+
+    public void LowerDoor ()
+    {
+        targetPosition = loweredPosition; //sets target position to lowered position
+    }
+}
---
+++ Assets/Aiden's shit/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit; //declare raycast variable
+        if (Physics.Raycast(transform.position, transform.forward, out hit)) //draws raycast from player position in the forward direction and outputs hit objects through hit
+        {
+            if (hit.transform.GetComponent<Button> () != null) //if the hit object is the button
+            {
+                hit.transform.GetComponent<Button> () .OnLook(); //onlook function from button called
+            }
+        }
+    }
+}
